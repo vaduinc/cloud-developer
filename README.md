933dcbeab3ad7ab69019e9d1272d5f66f8346a32
Jaime Valencia <jaimevadu@gmail.com>
2020-05-28 18:18:53 -0400

    documentation

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ content for Udacity's cloud developer nanodegree
 - Project 2 documentation `udacity-jaime-valencia-p2.pdf`
 
 - Project 3 documentation `udacity-jaime-valencia-p3.pdf`
+
+- Project 4 documentation `udacity-jaime-valencia-p4.pdf`
--- course-04/project/c4-final-project-solution/client/src/config.ts
+++ course-04/project/c4-final-project-solution/client/src/config.ts
@@ -1,10 +1,17 @@
 // TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = '...'
+
+// User: jaimetest@vaduinc.com		
+// Password: Jaimetest123
+
+// User: anotheruser@vaduinc.com
+// Password: Anotheruser123
+
+const apiId = 'w1g7a8b6uk'
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
 
 export const authConfig = {
   // TODO: Create an Auth0 application and copy values from it into this map
-  domain: '...',            // Auth0 domain
-  clientId: '...',          // Auth0 client id
+  domain: 'dev-1kjne1kz.auth0.com',            // Auth0 domain
+  clientId: 'S5D44Ilt2Ge4s57rhaaZUmMyNZ6rRiwQ',          // Auth0 client id
   callbackUrl: 'http://localhost:3000/callback'
 }
